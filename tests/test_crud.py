e50f15b9dcdcfdf1cc58aa2d08152171d3bf827f
smith <uriatecardenasmeyksmith@gmail.com>
2026-03-02 16:51:25 -0500

    Fix: final structure and line endings

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,9 +1,13 @@
 import sys
 import os
-# Esto le dice a Python que busque en la carpeta de arriba
+
+# Esto le dice a GitHub: "Sube un nivel y busca ahí el archivo crud.py"
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from crud import create, read, update, delete
 
 def test_create():
     assert create() == True
+
+def test_read():
+    assert read() == True
